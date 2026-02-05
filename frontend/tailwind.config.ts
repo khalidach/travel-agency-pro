b9b2e3a49f6b1b5f3f3aa408e3183abdcf2ab637
khalidach <khalidplaysg@gmail.com>
2026-02-05 04:00:43 +0100

    dark mode 1

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,8 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // Change from ["class"] to "class"
-  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
